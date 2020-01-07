051f184700bc7f04b396b03b5f534fa60a6f6777
Alapan Kar <alapankar27@gmail.com>
2020-01-07 10:10:58 +0530

    Update OneForAll.py

--- OneForAll.py
+++ OneForAll.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog, Text
+import os
 
 root=tk.Tk()
 
@@ -12,18 +13,24 @@ frame=tk.Frame(root, bg="white")
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
 def addapp():
+
+    for widget in frame.winfo_children():
+        widget.destroy()
+        
     filename=filedialog.askopenfilename(initialdir="/", title="Select File", filetypes=(("executables","*.exe"),("all files","*.*")))
     apps.append(filename)
     for app in apps:
         label=tk.Label(frame, text=app , bg="grey")
         label.pack()
 
-
+def runapps():
+    for app in apps:
+        os.startfile(app)
 
 openfile=tk.Button(root, text="Open File", fg="white", bg="#263d42", padx=10, pady=5, command=addapp)
 openfile.pack()
 
-runapps =tk.Button(root, text="Run Apps", fg="white", bg="#263d42", padx=10, pady=5)
+runapps =tk.Button(root, text="Run Apps", fg="white", bg="#263d42", padx=10, pady=5, command=runapps)
 runapps.pack()
 
 
